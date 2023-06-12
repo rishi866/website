90e93a60d6b16eef03a5f704ff79efd42e801ea1
rishi866 <136346648+rishi866@users.noreply.github.com>
2023-06-12 19:30:46 +0530

    Add files via upload

---
+++ mai.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+x=np.array(21)
+print(x)
+print(type(x))
+print(x.ndim)
---
+++ main.py
@@ -0,0 +1,25 @@
+# import numpy as np
+
+# a = numpy.array([[[12,2],[13,4]],[[14,16],[16,14]],[[18,10],[20,15]]])
+# b = numpy.array([[[8,8],[7,6]][[6,4],[2,10]],[[10,5]]])
+# print(a.ndim)
+# print(b.ndim)
+
+# a = numpy.array([3,4])
+# print(a)
+# # print(a.dtype)
+
+# x=np.array([1.2,2.2,3.2,4.2])
+# arr=x.astype('i')
+# print(arr)
+# # print(arr.dtype)
+
+# x=np.array([2,7,11,16,27,19,18,33])
+# y=x.view()
+# print(y.base)
+# print(x.base)
+
+# x=np.array([[2,7,11],[19,18,33]])
+# print(x.shape)
+
+while 
